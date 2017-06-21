e49e71defb590e4f98ccd450c7fa4a825633118e
hperez <hperez@webstaurantstore.com>
2017-06-21 11:41:53 -0400

    Log states to console

--- biometricClient.js
+++ biometricClient.js
@@ -1,31 +1,30 @@
 var Utilities = Utilities || {};
 
 Utilities.BiometricClient = function (endPoint, onOpen, onMessage, onError, onClose) {
+    console.log("BiometricClient");
+
     this.clientSocket = new WebSocket(endPoint);
 
-    console.log("BiometricClient.Constructor");
-
     this.Send = function (message) {
+        console.log("Before Send");
         this.clientSocket.send(message);
-    }
+        console.log("After Send");
+    }.bind(this);
 
     this.clientSocket.onopen = function (evt) {
-        console.log("BiometricClient.OnOpen");
-
+        console.log("OnOpen");
         if (onOpen != null) {
             onOpen(evt);
         }
     }.bind(this);
 
     this.clientSocket.onmessage = function (message) {
-        console.log("BiometricClient.OnMessage");
-
+        console.log("OnMessage");
         onMessage(message);
     }.bind(this);
 
     this.clientSocket.onclose = function (evt) {
-        console.log("BiometricClient.OnClose");
-
+        console.log("OnClose");
         if (onClose != null) {
             onClose(evt)
         }
@@ -33,6 +32,7 @@ Utilities.BiometricClient = function (endPoint, onOpen, onMessage, onError, onCl
     }.bind();
 
     this.Close = function () {
+        console.log("Close");
         if (this.clientSocket != null) {
             this.clientSocket.close();
             this.clientSocket = null;
